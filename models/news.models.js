72e12fd3d18c1bbbf03e6eafd8aa41e16c21a3f3
geoMx2022 <mwh17dur@gmail.com>
2022-07-05 15:46:32 +0100

    add refactor to 7-GET-api/articles/article_id with error handling

--- models/news.models.js
+++ models/news.models.js
@@ -8,7 +8,7 @@ exports.fetchTopics = () => {
 };
 
 exports.fetchArticleById = (article_id) => {
-    return db.query("SELECT * FROM articles WHERE article_id = $1;", [article_id]).then((article) => {
+    return db.query("SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.body, articles.created_at, articles.votes, COUNT(comments.article_id) AS comment_count FROM articles INNER JOIN comments ON articles.article_id = comments.article_id WHERE articles.article_id = $1 GROUP BY articles.article_id;", [article_id]).then((article) => {
         const articleData = article.rows[0]
         if (!articleData) {
             return Promise.reject({ status: 404, msg: "Not Found"});
