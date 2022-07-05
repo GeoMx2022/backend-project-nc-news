9dfe9f87c632b93be334386ca887e37beb4b60de
geoMx2022 <mwh17dur@gmail.com>
2022-07-05 15:10:10 +0100

    delete conditional logic on fetchTopics

--- models/news.models.js
+++ models/news.models.js
@@ -3,9 +3,6 @@ const db = require("../db/connection");
 exports.fetchTopics = () => {
     return db.query("SELECT * FROM topics;").then((topics) => {
         const topicsData = topics.rows;
-        if (!topicsData) {
-            return [];
-        }
         return topicsData;
     });
 };
