ac80ded8983aca3734376e9121125dd4e07c3bfc
geoMx2022 <mwh17dur@gmail.com>
2022-07-08 11:03:09 +0100

    delete test.sql and test.txt

--- test.sql
+++
@@ -1,3 +0,0 @@
-\c nc_news_test
-
-SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.created_at, articles.votes, CAST(COUNT(comments.article_id) AS INT) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id GROUP BY articles.article_id ORDER BY votes desc;
